0e98c8a34dc69d82ae091d27c38f08fd47dbc6fe
VitaliyTomchyk <vitatomchyk@gmail.com>
2022-12-16 11:19:02 +0100

    Changed sidebar button style

--- sidebar.py
+++ sidebar.py
@@ -4,26 +4,28 @@ from PIL import Image
 
 
 #images
-os_image = ctk.CTkImage(light_image=Image.open("images/open_sidebar.png"), \
-	size=(74, 74))
-cs_image = ctk.CTkImage(light_image=Image.open("images/close_sidebar.png"), \
-	size=(74, 74))
+#os_image = ctk.CTkImage(light_image=Image.open("images/open_sidebar.png"), \
+#	size=(74, 74))
+#cs_image = ctk.CTkImage(light_image=Image.open("images/close_sidebar.png"), \
+#	size=(74, 74))
 
 #sidebar
-sidebar = ctk.CTkFrame(root, width=230, height=500, corner_radius=0, \
-		fg_color='white')
+sidebar = ctk.CTkFrame(root, width=230, height=500, corner_radius=0)
 
 def open_sidebar():
 	sidebar.pack(side='left')
-	close_sidebar_btn = ctk.CTkButton(root, text='', image=cs_image, \
+	close_sidebar_btn = ctk.CTkButton(root, text='<', \
 	command=lambda: [sidebar.forget(), close_sidebar_btn.forget(), \
-	open_sidebar_btn.pack(side='left')], width=0, height=0, fg_color='white')
+	open_sidebar_btn.pack(side='left')], width=30, height=82, \
+	corner_radius=3, fg_color='gray86', font=ctk.CTkFont(size=20), \
+	text_color='black')
 	close_sidebar_btn.pack(side='left')
 	sidebar.pack_propagate(False)
 	return
 
 #open sidebar button
-open_sidebar_btn = ctk.CTkButton(root, text='', image=os_image, \
+open_sidebar_btn = ctk.CTkButton(root, text='>', \
 		command=lambda: [open_sidebar(), open_sidebar_btn.forget()], \
-		width=0, height=0, fg_color='white')
+		width=30, height=82, corner_radius=3, fg_color='black', \
+		font=ctk.CTkFont(size=20))
 open_sidebar_btn.pack(side='left')
