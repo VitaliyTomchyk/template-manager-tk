e6b4c3cbd3660734f36f55333ae8493341b70521
VitaliyTomchyk <vitatomchyk@gmail.com>
2022-12-18 19:28:10 +0100

    added text frame

--- tabs/create_template_page.py
+++ tabs/create_template_page.py
@@ -3,10 +3,15 @@ import tkinter as tk
 from window import root
 
 
-frame = ctk.CTkFrame(root, width=1320, height=1048)
+frame = ctk.CTkFrame(root, width=1750, height=1080, border_width=1.5)
 
 def create_template_page():
 	frame.pack(side='right')
-	label = ctk.CTkLabel(frame, text='create me jijijiji')
-	label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
+	textbox = ctk.CTkTextbox(frame, width=1000, height=706, border_width=1.5, \
+		font=("Roboto", 18))
+	textbox.place(relx=0.40, rely=0.5, anchor=tk.CENTER)
+
+	save_btn = ctk.CTkButton(frame, width=150, height=60, text="Next step", \
+		font=("Roboto", 15))
+	save_btn.place(relx=0.892, rely=0.9, anchor=tk.CENTER)
